9ec4ae0f90ad553e928db0c14dd149842beb880c
Asus ROG Laptop <david@fault5.com>
2018-01-11 14:30:47 -0600

    Removed include for DB connection; Added full width parallax; made header bg transparent; update one page layout;

--- coming-soon.php
+++ coming-soon.php
@@ -1,36 +1,50 @@
+ <!-- connects to database used for subscribers -->
 <html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<base href="http://localhost/fault-five/">
+<base target="_blank" href="http://localhost/fault-five/">
 
 <!doctype html>
-<!--<base href="https://localhost/fault-five" target="_blank">-->
-<head> 
-	<title>Fault Five Media Group | One of the Best Website Companies in Wisconsin offering SEO for Wisconsin Small Businesses</title>
-  <meta name="Description" content="Fault Five Media Group is a Milwaukee Website Company that Builds Profitable Websites backed by Purposeful Search Engine Optimization. Visit to Learn More!">
-	<link href="style.min.css" rel="stylesheet" type="text/css">
-		<?php include ('includes/head.php'); ?>
-	  
 
-	
+<head>
+	<title>Fault Five Media Group | One of the Best Website Companies in Wisconsin offering SEO for Wisconsin Small Businesses</title>
+	<meta name="Description" content="Fault Five Media Group is a Milwaukee Website Company that Builds Profitable Websites backed by Purposeful Search Engine Optimization. Visit to Learn More!">
+	<link href="style.min.css" rel="stylesheet" type="text/css">
+	<?php include ('includes/head.php'); ?>
+
+	<!-- head tag is closed within include: head.php -->
+
 	<body>
-		
 		<div class="header">
 			<div class="header__stripe">
 			</div>
 			<span class="header__title">Fault Five Media Group</span>
-		</div>
-		
-		
-		
-			<form class="form" action="subscribe.php" method="post">
-				<h4>Stay in The Loop</h4>
-				<p>We will continue to send out our monthly emails providing tips and tricks on how to enhance your website's value.</p>
-			  <div class="form__functions">
-				<input class="form__input" name="name" placeholder="Name" />
-				<input class="form__input" name="email" placeholder="Email" />
-				<button class="form__input" type="submit" name="" value="send"><span>Send </span></button>
-				
-				</div>
-			</form>
-		
+			</div>
+
+			<div class="main parallax">
+
+			</div>
+
+
+
+
+
+			<footer>
+
+
+				<form class="form" action="subscribe.php" method="post">
+					<h4>Stay in The Loop</h4>
+					<p>We will continue to send out our monthly emails providing tips and tricks on how to enhance your website's value.</p>
+					<div class="form__functions">
+						<input class="form__input" name="name" placeholder="Name"/>
+						<input class="form__input" name="email" placeholder="Email"/>
+						<button class="form__input" type="submit" name="" value="send"><span>Send </span></button>
+
+					</div>
+				</form>
+
+			</footer>
+
+			<div class="banner banner--footer"> </div>
 
 	</body>
 
