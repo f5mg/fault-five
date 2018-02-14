94208a6c1f40e3727bd63720868de2ae2a5e892a
f5mg <33160174+f5mg@users.noreply.github.com>
2018-02-13 22:54:38 -0600

    Removed Alpha Attribute Mixin;
    Created new Nested Element main__message;
    Added Header text to main_message;
    Put Form inside main__message;

--- coming-soon.php
+++ coming-soon.php
@@ -1,4 +1,4 @@
- <?php include ('php/db.php'); ?>
+
  <!-- connects to database used for subscribers -->
 <html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
 
@@ -8,6 +8,7 @@
 	<title>Fault Five Media Group | One of the Best Website Companies in Wisconsin offering SEO for Wisconsin Small Businesses</title>
 	<meta name="Description" content="Fault Five Media Group is a Milwaukee Website Company that Builds Profitable Websites backed by Purposeful Search Engine Optimization. Visit to Learn More!">
 	<link href="style.min.css" rel="stylesheet" type="text/css">
+	<link href="css/animate.css" rel="stylesheet" type="text/css">
 	<?php include ('includes/head.php'); ?>
 
 	<!-- head tag is closed within include: head.php -->
@@ -20,7 +21,22 @@
 			</div>
 
 			<div class="main parallax">
-
+				
+				<div class="main__message">
+					<h2>New Site Coming!</h2>
+					<h4>We are working on our new website using current standards. If you want to contact us please use the form below of email us at hello@fault5.com</h4>
+					
+					<form class="form" action="subscribe.php" method="post">
+					<h4>Stay in The Loop</h4>
+					<p>We will continue to send out our monthly emails providing tips and tricks on how to enhance your website's value.</p>
+					<div class="form__functions">
+						<input class="form__input" name="name" placeholder="Name"/>
+						<input class="form__input" name="email" placeholder="Email"/>
+						<button class="form__input" type="submit" name="" value="send"><span>Send </span></button>
+					</div>
+				</form>
+					
+				</div>
 			</div>
 
 
@@ -35,21 +51,16 @@
 					<li><a href="#"></a></li>
 				</ul>
 				</div>
+					
 				<div class="flexed__item"></div>
 				
-				<form class="form" action="subscribe.php" method="post">
-					<h4>Stay in The Loop</h4>
-					<p>We will continue to send out our monthly emails providing tips and tricks on how to enhance your website's value.</p>
-					<div class="form__functions">
-						<input class="form__input" name="name" placeholder="Name"/>
-						<input class="form__input" name="email" placeholder="Email"/>
-						<button class="form__input" type="submit" name="" value="send"><span>Send </span></button>
-					</div>
-				</form>
+			
 
 		</div><!-- div.footer -->
 
-			<div class="banner banner--footer"> </div>
+			<div class="banner banner--footer"> 
+				
+				</div>
 
 	</body>
 
