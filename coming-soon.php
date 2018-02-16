b378cc9a62f639070122a91b2450882aea80c201
f5mg <33160174+f5mg@users.noreply.github.com>
2018-02-15 22:10:59 -0600

    - Added overlay element directly inside Main
    - Moved form out of Main__message
    - Changed flex-flow properties for main__message
    - Created 2 flexed_items inside of main_message (column on mobile, row on desktop)
    - Changed headers to H2 inside main__message flexed__item

--- coming-soon.php
+++ coming-soon.php
@@ -8,7 +8,6 @@
 	<title>Fault Five Media Group | One of the Best Website Companies in Wisconsin offering SEO for Wisconsin Small Businesses</title>
 	<meta name="Description" content="Fault Five Media Group is a Milwaukee Website Company that Builds Profitable Websites backed by Purposeful Search Engine Optimization. Visit to Learn More!">
 	<link href="style.min.css" rel="stylesheet" type="text/css">
-	<link href="css/animate.css" rel="stylesheet" type="text/css">
 	<?php include ('includes/head.php'); ?>
 
 	<!-- head tag is closed within include: head.php -->
@@ -18,28 +17,40 @@
 			<div class="header__stripe">
 			</div>
 			<span class="header__title">Fault Five Media Group</span>
-			</div>
+			<div class="main-nav">
+			
+			</div><!--main-nav-->
+			</div><!--header-->
 
 			<div class="main parallax">
+				<div class="overlay">
 				
-				<div class="main__message">
-					<h2>New Site Coming!</h2>
-					<h4>We are working on our new website using current standards. If you want to contact us please use the form below of email us at hello@fault5.com</h4>
+					<div class="main__message">
 					
-					<form class="form" action="subscribe.php" method="post">
-					<h4>Stay in The Loop</h4>
-					<p>We will continue to send out our monthly emails providing tips and tricks on how to enhance your website's value.</p>
-					<div class="form__functions">
-						<input class="form__input" name="name" placeholder="Name"/>
-						<input class="form__input" name="email" placeholder="Email"/>
-						<button class="form__input" type="submit" name="" value="send"><span>Send </span></button>
-					</div>
-				</form>
-					
-				</div>
-			</div>
+						<div class="flexed__item">
+						<h3>Invest in Your Online Presence</h3>
+						
+						</div>
+						
+						<div class="flexed__item">
+						<h3>Draw Customers to Your Website</h3>
+						</div>
+						
+						
+						
+					</div><!--flexed-->
+				</div><!--overlay-->
+			</div><!--main-->
 
-
+			<form class="form" action="subscribe.php" method="post">
+				<h4>Stay in The Loop</h4>
+				<p>We will continue to send out our monthly emails providing tips and tricks on how to enhance your website's value.</p>
+				<div class="form__functions">
+					<input class="form__input" name="name" placeholder="Name"/>
+					<input class="form__input" name="email" placeholder="Email"/>
+					<button class="form__input" type="submit" name="" value="send"><span>Send </span></button>
+				</div><!--form__funtions-->
+			</form>
 
 
 
@@ -47,7 +58,7 @@
 
 				<div class="flexed__item">
 				<ul class="list">
-					<h5>
+				 
 					<li><a href="#"></a></li>
 				</ul>
 				</div>
